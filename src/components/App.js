f470d85cf3c3fc1ebb0a985e87884c58349ecedc
Denis Gabbasov <gabbas.denn@mail.ru>
2023-02-20 18:51:54 +0300

    feat:добавить компоненты попапов и фукнционал открытие и закрытия через состояния

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,78 @@
+import React from 'react';
+
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
+import PopupWithForm from './PopupWithForm';
 
 
 function App() {
+
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
+  
+  const handleEditAvatarClick = () => {
+    setIsEditAvatarPopupOpen(true);
+    document.querySelector('.popup_type_edit-avatar').classList.add('popup_opened');
+  }
+
+  const handleEditProfileClick = () => {
+    setIsEditProfilePopupOpen(true);
+    document.querySelector('.popup_type_profile').classList.add('popup_opened');
+  }
+
+  const handleAddPlaceClick = () => {
+    setIsAddPlacePopupOpen(true);
+    document.querySelector('.popup_type_add').classList.add('popup_opened');
+  }
+
+  const closeAllPopups = () => {
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+  }
+
   return (
     <div className='page'>
 
       <Header/>
 
-      <Main/>
+      <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick}/>
 
       <Footer/>
 
-      <section className="popup popup_type_profile">
+      <PopupWithForm 
+        name="profile" 
+        title="Редактировать профиль" 
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}>
+        <input required id="name-profile-input" name="name" type="text" className="popup__input popup__input_field_name" placeholder="Имя" value="Жак-Ив Кусто" minlength="2" maxlength="40"/>
+        <span class="popup__input-error name-profile-input-error"></span>
+        <input required id="post-input" name="about" type="text" class="popup__input popup__input_field_post" placeholder="О себе" value="Исследователь океана" minlength="2" maxlength="200"/>
+        <span class="popup__input-error post-input-error"></span>
+      </PopupWithForm>
+      <PopupWithForm 
+        name="add" 
+        title="Новое место" 
+        isOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}>
+        <input required id="name-place-input" name="name" type="text" className="popup__input popup__input_field_place-name" placeholder="Название" minlength="2" maxlength="30"/>
+        <span className="popup__input-error name-place-input-error"></span>
+        <input required id="link-place-input" name="link" type="url" className="popup__input popup__input_field_place-link" placeholder="Ссылка на картинку"/>
+        <span className="popup__input-error link-place-input-error"></span>
+        <button type="submit" className="popup__button-form">Создать</button>
+      </PopupWithForm>
+      <PopupWithForm 
+        name="edit-avatar" 
+        title="Обновить аватар" 
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}>
+        <input required id="link-avatar-input" name="avatar" type="url" className="popup__input popup__input_field_avatar-link" placeholder="Ссылка на картинку"/>
+        <span className="popup__input-error link-avatar-input-error"></span>
+        <button type="submit" className="popup__button-form">Сохранить</button>
+      </PopupWithForm>
+      {/* <section className="popup popup_type_profile">
         <div className="popup__container">
           <form name="editForm" className="popup__form popup__form_edit" novalidate>
             <h2 className="popup__title-form">Редактировать профиль</h2>
@@ -27,9 +86,9 @@ function App() {
           </form>
           <button type="button" aria-label="Закрыть" class="popup__close"></button>
         </div>
-      </section>
+      </section> */}
 
-      <section className="popup popup_type_add">
+      {/* <section className="popup popup_type_add">
         <div className="popup__container">
           <form name="addForm" className="popup__form popup__form_add" novalidate>
             <h2 className="popup__title-form">Новое место</h2>
@@ -41,7 +100,7 @@ function App() {
           </form>
           <button type="button" aria-label="Закрыть" className="popup__close"></button>
         </div>
-      </section>
+      </section> */}
 
       <section className="popup popup_type_image">
         <div className="popup__container-image">
@@ -62,7 +121,7 @@ function App() {
           <button type="button" aria-label="Закрыть" className="popup__close"></button>
         </div>
       </section>
-
+{/* 
       <section className="popup popup_type_edit-avatar">
         <div className="popup__container">
           <form name="addForm" className="popup__form popup__form_edit-avatar" novalidate>
@@ -73,7 +132,7 @@ function App() {
           </form>
           <button type="button" aria-label="Закрыть" className="popup__close"></button>
         </div>
-      </section>
+      </section> */}
     </div>
        
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,22 @@
 import avatar from '../images/avatar.png'
 
 
-function Main() {
-
-  
+function Main(props) {
 
   return (
     <main className="content">
       <section className="info page__info">
         <div className="info__profile">
-          <div className="info__profile-avatar-wrapper">
+          <div onClick={props.onEditAvatar} className="info__profile-avatar-wrapper">
             <img src={avatar} alt="Аватарка профиля" className="info__profile-avatar"/>
           </div>
           <div className="info__profile-descr">
             <h1 className="info__profile-name">Жак-Ив Кусто</h1>
-            <button type="button" aria-label="Редактировать" className="info__edit-button"></button>
+            <button onClick={props.onEditProfile} type="button" aria-label="Редактировать" className="info__edit-button"></button>
           </div>
           <p className="info__profile-post">Исследователь океана</p>
         </div>
-        <button type="button" aria-label="Закрыть" className="info__add-button"></button>
+        <button onClick={props.onAddPlace} type="button" aria-label="Закрыть" className="info__add-button"></button>
       </section>
 
       <section className="elements page__elements">
