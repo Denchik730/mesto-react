077dde8a356c7d6500970e084b6142e6ab239119
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-23 18:25:46 +0300

    feat:добавить новый компонент Popup и реализовать в нем функциональность закрытия по оверлею и Escape

--- src/components/App.js
+++ src/components/App.js
@@ -139,24 +139,6 @@ function App() {
     setSelectedCard(null);
   }
 
-  const isOpen = isEditAvatarPopupOpen || isEditProfilePopupOpen || isAddPlacePopupOpen || selectedCard || willDeleteCard;
-
-  React.useEffect(() => {
-    function closeEscape(evt) {
-      if (evt.key === `Escape`) {
-        closeAllPopups();
-      }
-    }
-
-    if (isOpen) {
-      document.addEventListener('keydown', closeEscape);
-    }
-
-    return () => {
-      document.removeEventListener('keydown', closeEscape);
-    }
-  }, [isOpen]);
-
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <div className='page'>
