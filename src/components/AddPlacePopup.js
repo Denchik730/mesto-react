b907081c623563624f1597c83c5d89ca56d2035e
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-24 18:46:39 +0300

    feat:добавить валидацию на форму добавления новой карточки

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -29,29 +29,30 @@ function AddPlacePopup({isOpen, onClose, onAddPlace, isLoadingRequest}) {
       isLoadingRequest={isLoadingRequest}
       isOpen={isOpen}
       onClose={onClose}
-      onSubmit={handleSubmit}>
+      onSubmit={handleSubmit}
+      isDisabled={!isValid}>
       <input
         required
         id="name-place-input"
         name="name"
         type="text"
-        className="popup__input popup__input_field_place-name"
+        className={`popup__input ${errors.name && 'popup__input_type_error'} popup__input_field_name`}
         placeholder="Название"
         minLength="2"
         maxLength="30"
         value={values.name || ''}
         onChange={handleChange}/>
-      <span className="popup__input-error name-place-input-error"></span>
+      <span className={`popup__input-error ${errors.name && 'popup__input-error_active'} name-profile-input-error`}>{errors.name || ''}</span>
       <input
         required
         id="link-place-input"
         name="link"
         type="url"
-        className="popup__input popup__input_field_place-link"
+        className={`popup__input ${errors.link && 'popup__input_type_error'} popup__input_field_name`}
         placeholder="Ссылка на картинку"
         value={values.link || ''}
         onChange={handleChange}/>
-      <span className="popup__input-error link-place-input-error"></span>
+      <span className={`popup__input-error ${errors.link && 'popup__input-error_active'} name-profile-input-error`}>{errors.link || ''}</span>
     </PopupWithForm>
   );
 }
